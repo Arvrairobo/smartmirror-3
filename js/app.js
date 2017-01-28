7b4de0fc5a3bbdb773dfb393395389db1e088e01
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2017-01-28 16:37:57 -0500

    Added date and time widget

--- js/app.js
+++ js/app.js
@@ -10,4 +10,51 @@ $(document).ready(function() {
 		$('#weather #wind').html(w.current.skytext);
 		$('#weather #wind').prepend("<i class='wi wi-yahoo-"+w.current.skycode+"'></i>");
 	});
+	
+	renderClock();
+	renderDate();
 });
+
+function renderClock(){
+	var currentTime = new Date();
+	var period = "AM";
+	var hour = currentTime.getHours();
+	var min = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+	
+	if(hour == 0){
+		hour = 12;
+	}else if(hour > 12){
+		hour = hour - 12
+		period = "PM"
+	}
+	
+	if(hour < 10){
+		hour = "0" + hour;
+	}
+	
+	if(min < 10){
+		min = "0" + min;
+	}
+	
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	
+	$('#clock').html(hour + ":" + min + ":" + seconds + " " + period);
+	setTimeout('renderClock()', 1000);
+}
+
+function renderDate(){
+	var monthNames = [  "Jan.", "Feb.", "Mar.",
+  "Apr.", "May", "June", "July",
+  "Aug.", "Sept.", "Oct.",
+  "Nov.", "Dec."];
+	
+	var date = new Date();
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	$('#date').html(monthNames[monthIndex] + " " + day + ", " + year);
+}
