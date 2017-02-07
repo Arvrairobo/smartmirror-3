5940b92c6b6564022017c162a0127153dd8f8c14
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2017-02-07 00:23:12 -0500

    Added explanation comments.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
 	$.get('/config', function(ret){
 		var modulesToLoad = [];
 
+		//loop through return object
+		//grab only the one's with scripts
 		for(var item in ret[0]){
 			if(!(ret[0][item].script.trim() === "")){
 			 	modulesToLoad.push(ret[0][item]);
 			}
 		}
 		
+		//load each widget
+		//General concept of how each widget will be loading:
+		//A wrapper div will be created for each widget at specified spot
+		//within each individual script, they will only add elements to the wrapper
 		for(var i = 0; i < modulesToLoad.length; i++){
 			addWidgetWrapper(modulesToLoad[i]);
 			addWidget(modulesToLoad[i].script);
