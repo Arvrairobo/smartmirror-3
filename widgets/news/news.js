56465aa8e58593aa6c6017bcdab9e3af3ac78ff7
Jose Ruiz <jmaruiz5@gmail.com>
2017-02-20 22:18:15 -0500

    News: remove delay, use promise

--- widgets/news/news.js
+++ widgets/news/news.js
@@ -13,22 +13,23 @@ $.get('/news', function(response){
 
 function addNewsWidget(headlines) {
   var i = 0;
-  $('#news').append('<div class="news-title"">Lastest in <strong>Google News<strong></div>');
-  $('#news').append('<div class="news-item" style="display:none;">'+headlines[i].title+'</div>');
-  $('#news').append('<div class="news-desc" style="display:none;">'+headlines[i].description+'</div>');
-  $('#news .news-item').fadeIn(250).delay(4500).fadeOut(250);
+  $('#news').append('<div class="news-header"">Lastest in <strong>Google News<strong></div>');
+  $('#news').append('<div class="news-item" style="display:none;"><div class="news-title"></div><div class="news-desc"></div></div>');
+  $('#news .news-title').html(headlines[i].title);
   if (headlines[i].description !== 'null') {
-    $('#news .news-desc').fadeIn(250).delay(4500).fadeOut(250);
+    $('#news .news-desc').html(headlines[i].description);
   }
+  $('#news .news-item').fadeIn(250);
+
+
   setInterval(function() {
-    if (i < headlines.length - 1) { i++; } else { i = 0; }
-    $('#news .news-item').html('<div class="news-item" style="display:none;">'+headlines[i].title+'</div>');
-    if (headlines[i].description !== null) {
-      $('#news .news-desc').html('<div class="news-desc" style="display:none;">'+headlines[i].description+'</div>');
-      $('#news .news-desc').fadeIn(250).delay(4500).fadeOut(250);
-    }
-    $('#news .news-item').fadeIn(250).delay(4500).fadeOut(250);
-
-
-  }, 5000);
+    $('#news .news-item').fadeOut(250).promise().done(function(){
+      if (i < headlines.length - 1) { i++; } else { i = 0; }
+      $('#news .news-title').html(headlines[i].title);
+      if (headlines[i].description !== null) {
+        $('#news .news-desc').html(headlines[i].description);
+      }
+      $('#news .news-item').fadeIn(250);
+    });
+  }, 10000);
 }
