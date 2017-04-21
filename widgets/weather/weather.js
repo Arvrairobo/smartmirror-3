91a2b8b1d5be8692ad85c5e3ad9db0af299558ed
Jose Ruiz <jmaruiz5@gmail.com>
2017-04-21 15:29:06 -0400

    remove skycons, too small and render poorly on RPi

--- widgets/weather/weather.js
+++ widgets/weather/weather.js
@@ -12,14 +12,22 @@ $.getJSON({
 function wrapperSetup(weatherdata){
 	$('#weather').html(`<div id="temp"></div>
 		<div id="location"></div>
-		<div id="wind"><canvas id="todayWeatherIcon"></canvas></div>
+		<div id="wind"><i class="wi"></i></div>
 		<div id="forecast">
 			<div class="row">
+				<div class="col">
+					<div id="day0">
+						<div id="day"></div>
+						<div id="high"></div>
+						<div id="skyIcon"></div>
+						<div id="low"></div>
+					</div>
+				</div>
 				<div class="col">
 					<div id="day1">
 						<div id="day"></div>
 						<div id="high"></div>
-						<div id="skyIcon"><canvas id="day1ForcastIcon" class="forecastIcon"></canvas></div>
+						<div id="skyIcon"></div>
 						<div id="low"></div>
 					</div>
 				</div>
@@ -27,7 +35,7 @@ function wrapperSetup(weatherdata){
 					<div id="day2">
 						<div id="day"></div>
 						<div id="high"></div>
-						<div id="skyIcon"><canvas id="day2ForcastIcon" class="forecastIcon"></canvas></div>
+						<div id="skyIcon"></div>
 						<div id="low"></div>
 					</div>
 				</div>
@@ -35,15 +43,7 @@ function wrapperSetup(weatherdata){
 					<div id="day3">
 						<div id="day"></div>
 						<div id="high"></div>
-						<div id="skyIcon"><canvas id="day3ForcastIcon" class="forecastIcon"></canvas></div>
-						<div id="low"></div>
-					</div>
-				</div>
-				<div class="col">
-					<div id="day4">
-						<div id="day"></div>
-						<div id="high"></div>
-						<div id="skyIcon"><canvas id="day4ForcastIcon" class="forecastIcon"></canvas></div>
+						<div id="skyIcon"></div>
 						<div id="low"></div>
 					</div>
 				</div>
@@ -55,25 +55,21 @@ function wrapperSetup(weatherdata){
 
 function addWidget(weather){
 	var date = new Date();
-	var skycons = new Skycons({"color":"white"});
 	var dayOfWeek = ["Sun","Mon", "Tues", "Wed","Thur", "Fri", "Sat"];
 	var todayDayIndex =  date.getDay();
 	var temp = 0;
-	
+
 	$('#weather #temp').html(Math.ceil(parseInt(weather.today.temperature)) + "&deg;F");
 	$('#weather #location').html("Toledo, OH");
-	skycons.add($('#todayWeatherIcon')[0], weather.today.icon);
-	skycons.play();
-	$('#weather #wind').append(weather.today.weatherDescription);
+	$('#weather #wind').html(weather.today.weatherDescription);
+	$('#weather #wind').prepend("<i class='wi wi-forecast-io-"+weather.today.icon+"'></i>");
 
-	for(var i = 1; i <= 4; i++){
+	for (var i = 0; i < 4; i++) {
 		var dayTag = "#day" + i;
-		var forceCastIconTag = "#day" + i + "ForcastIcon";
-		$('#forecast ' + dayTag + ' #high').html(Math.ceil(parseInt(weather.forecast[i - 1].high)));
-		var forecastSkycon = new Skycons({"color":"white"});
-		forecastSkycon.add($(forceCastIconTag)[0], weather.forecast[i - 1].icon);
-		forecastSkycon.play();
-		$('#forecast ' + dayTag + ' #low').html(Math.ceil(parseInt(weather.forecast[i - 1].low)));
+		$('#forecast ' + dayTag + ' #day').html(weather.forecast[i].shortday);
+		$('#forecast ' + dayTag + ' #high').html(Math.ceil(parseInt(weather.forecast[i].high)));
+		$('#forecast ' + dayTag + ' #skyIcon').html("<i class='wi wi-forecast-io-"+weather.forecast[i].icon+"'></i>");
+		$('#forecast ' + dayTag + ' #low').html(Math.ceil(parseInt(weather.forecast[i].low)));
 		if((todayDayIndex + i) > 6){
 			$('#forecast ' + dayTag + ' #day').html(dayOfWeek[temp]);
 			temp++;
@@ -81,7 +77,7 @@ function addWidget(weather){
 			$('#forecast ' + dayTag + ' #day').html(dayOfWeek[todayDayIndex + i]);
 		}
 	}
-	
+
 }
 
 function updateWeather() {
